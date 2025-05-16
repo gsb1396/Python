e2b13447034aa6db535e3391408d26459d7cc5b6
gsb1396 <gabrielsabrasil96@gmail.com>
2025-05-16 11:39:11 -0400

    Tipos Primitivos. Exercícios 1, 2, 3, 4

---
+++ exercicios/lista de exercicios/ex003.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um segundo valor: '))
+r = n1 + n2
+
+print('O valor entre {} e {} é {}'.format(n1, n2, r))
+
+#   Tipos Primitivos
+#       int = Número Inteiro. 4, 5, 6
+#       float = Números Reais ou Pontos Flutuantes. 1.0, 2.0, 3.0 
+#       bool = Valores Lógicos ou Booleanos. Verdadeiro ou Falso
+#       str = Caracter ou String. C, a, casa
---
+++ exercicios/pythontest/aula06A.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite um valor: '))
+r = n1 + n2
+
+print('A soma entre {1} e {0} vale {2}'.format(n1, n2, r))
